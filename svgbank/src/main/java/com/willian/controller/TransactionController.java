969ec1f2cf3db4815c818408d124af2705bba550
Willian Guedes <willian.wg70@gmail.com>
2023-02-16 02:45:21 +0000

    feat: add basic transaction controller

---
+++ svgbank/src/main/java/com/willian/controller/TransactionController.java
@@ -0,0 +1,15 @@
+package com.willian.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/transaction")
+public class TransactionController {
+    
+    @GetMapping
+    public String test(){
+        return "Transaction path";
+    }
+}
