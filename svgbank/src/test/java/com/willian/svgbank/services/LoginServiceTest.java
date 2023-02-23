e7cde285ad90aab41ff8d86e326ed55895e712a3
Willian Guedes <willian.wg70@gmail.com>
2023-02-23 01:15:08 +0000

    test: add fail login test

--- svgbank/src/test/java/com/willian/svgbank/services/LoginServiceTest.java
+++ svgbank/src/test/java/com/willian/svgbank/services/LoginServiceTest.java
@@ -1,6 +1,7 @@
 package com.willian.svgbank.services;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import org.junit.jupiter.api.Test;
 
@@ -35,5 +36,28 @@ public class LoginServiceTest {
         User user = loginService.execute(loginDto);
 
         assertNotNull(user.getId());
+    }
+
+    @Test
+    public void should_not_be_able_to_login(){
+        IUserRepository userRepository = new inMemoryUserRepository();
+        IPasswordEncoder passwordEncoder = new PasswordEncoderTest();
+        LoginService loginService = new LoginService(userRepository, passwordEncoder);
+
+        User newUser = new User();
+        newUser.setEmail("willian@willain.com");
+        newUser.setName("willian");
+        newUser.setPassword("123456");
+
+        userRepository.save(newUser);
+
+        LoginDto loginDto = new LoginDto();
+        loginDto.setEmail("willian@willain.com");
+        loginDto.setPassword("12345");
+
+
+        User user = loginService.execute(loginDto);
+
+        assertNull(user);
     }
 }
